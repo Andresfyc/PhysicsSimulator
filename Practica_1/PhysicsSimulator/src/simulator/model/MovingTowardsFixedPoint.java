c969b0d7f73006282fcc65b5bc2f22ca2c567085
Sodoka <alexandracideca@gmail.com>
2021-03-10 19:59:30 +0100

    MovingTowardsFixedPoint Terminar...

--- Practica_1/PhysicsSimulator/src/simulator/model/MovingTowardsFixedPoint.java
+++ Practica_1/PhysicsSimulator/src/simulator/model/MovingTowardsFixedPoint.java
@@ -6,19 +6,21 @@ import java.util.List;
 
 public class MovingTowardsFixedPoint implements ForceLaws {
 
-    static private final double g=9.81;
+    private double g=9.81;
+    private Vector2D _c;
 
-    public MovingTowardsFixedPoint() {
-        super();
+
+    public MovingTowardsFixedPoint(double g, Vector2D c) {
+        this.g = g;
+        //this._c = _c;
+        _c =new Vector2D(c);
     }
 
     @Override
     public void apply(List<Body> bs) {
 
-
         for (Body b: bs ) {
-
-            //b.addForce(c.minus(b.getPosition()).direction().scale(g*b.getMass()));
+            b.addForce(_c.minus(b.getPosition()).direction().scale(g*b.getMass()));
         }
 
     }
