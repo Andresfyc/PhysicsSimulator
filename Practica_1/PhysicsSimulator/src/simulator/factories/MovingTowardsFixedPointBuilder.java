a280f72a43252bc27537529e7645568e16528a5d
Sodoka <alexandracideca@gmail.com>
2021-04-03 14:29:40 +0200

    Factorias

--- Practica_1/PhysicsSimulator/src/simulator/factories/MovingTowardsFixedPointBuilder.java
+++ Practica_1/PhysicsSimulator/src/simulator/factories/MovingTowardsFixedPointBuilder.java
@@ -1,5 +1,6 @@
 package simulator.factories;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import simulator.misc.Vector2D;
 import simulator.model.ForceLaws;
@@ -12,16 +13,28 @@ public class MovingTowardsFixedPointBuilder extends Builder<ForceLaws>{
         super("mtcp", "Moving Towards Fixed Point");
     }
 
-    @Override
-    JSONObject createData() {
-        JSONObject data = new JSONObject();
-        return data;
-    }
 
     @Override
     protected ForceLaws createTheInstance(JSONObject data) {
+
+        JSONArray V=data.getJSONArray("c");
+        Vector2D c = new Vector2D(V.getDouble(0),V.getDouble(1));
         double g = data.has("_g") ? data.getDouble("_g") : 9.81;
-        Vector2D c = data.has("c") ? data.getJSONArray("c") : [0,0]);
+        //Vector2D c = data.has("c") ? data.getJSONArray("c") : [0,0]);
+
         return new MovingTowardsFixedPoint(g,c);
+    }
+
+    @Override
+    JSONObject createData() {
+        JSONObject ob=new JSONObject();
+        JSONArray data = new JSONArray();
+
+        data.put(0.0);
+        data.put(0.0);
+        ob.put("c",data);
+
+        ob.put("g",9.81);
+        return ob;
     }
 }
